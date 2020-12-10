951475d9e985756c18ef165bc3fe3b73a9bdb160
n1290088 <71298361+n1290088@users.noreply.github.com>
2020-12-10 14:04:19 +0900

    Add files via upload

---
+++ 電卓/script.js
@@ -0,0 +1,18 @@
+function a(v){
+    document.querySelector('input').value += v
+}
+
+function b(){
+    var l = document.querySelector('input').value
+    document.querySelector('input').value = l.slice(0,-1)
+}
+
+function cl(v){
+    document.querySelector('input').value = v
+}
+
+function calc(){
+    const l = document.querySelector('input').value
+    const f = new Function('return ' + l)
+    cl(f().toString())
+}
